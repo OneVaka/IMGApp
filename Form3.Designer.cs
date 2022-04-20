caf23dbe518bbd9bf8a33c9af4ebde299d9e2fba
OneVaka <gorelow.gore2012@yandex.ru>
2022-04-20 14:21:50 +0300

    Added Sauval method + progressBar (no optimization)

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -37,15 +37,17 @@ namespace IMGApp
             this.buttonWolf = new System.Windows.Forms.Button();
             this.buttonBradley = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(10, 9);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(688, 691);
+            this.pictureBox1.Size = new System.Drawing.Size(602, 518);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -53,9 +55,10 @@ namespace IMGApp
             // 
             // buttonGavr
             // 
-            this.buttonGavr.Location = new System.Drawing.Point(725, 31);
+            this.buttonGavr.Location = new System.Drawing.Point(634, 23);
+            this.buttonGavr.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonGavr.Name = "buttonGavr";
-            this.buttonGavr.Size = new System.Drawing.Size(94, 29);
+            this.buttonGavr.Size = new System.Drawing.Size(82, 22);
             this.buttonGavr.TabIndex = 1;
             this.buttonGavr.Text = "Gavrilov";
             this.buttonGavr.UseVisualStyleBackColor = true;
@@ -63,9 +66,10 @@ namespace IMGApp
             // 
             // buttonOtsu
             // 
-            this.buttonOtsu.Location = new System.Drawing.Point(725, 82);
+            this.buttonOtsu.Location = new System.Drawing.Point(634, 62);
+            this.buttonOtsu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonOtsu.Name = "buttonOtsu";
-            this.buttonOtsu.Size = new System.Drawing.Size(94, 29);
+            this.buttonOtsu.Size = new System.Drawing.Size(82, 22);
             this.buttonOtsu.TabIndex = 2;
             this.buttonOtsu.Text = "Otsu";
             this.buttonOtsu.UseVisualStyleBackColor = true;
@@ -73,9 +77,10 @@ namespace IMGApp
             // 
             // buttonNiblack
             // 
-            this.buttonNiblack.Location = new System.Drawing.Point(725, 141);
+            this.buttonNiblack.Location = new System.Drawing.Point(634, 106);
+            this.buttonNiblack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonNiblack.Name = "buttonNiblack";
-            this.buttonNiblack.Size = new System.Drawing.Size(94, 29);
+            this.buttonNiblack.Size = new System.Drawing.Size(82, 22);
             this.buttonNiblack.TabIndex = 3;
             this.buttonNiblack.Text = "Niblack";
             this.buttonNiblack.UseVisualStyleBackColor = true;
@@ -83,9 +88,10 @@ namespace IMGApp
             // 
             // buttonSauvola
             // 
-            this.buttonSauvola.Location = new System.Drawing.Point(725, 189);
+            this.buttonSauvola.Location = new System.Drawing.Point(634, 142);
+            this.buttonSauvola.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonSauvola.Name = "buttonSauvola";
-            this.buttonSauvola.Size = new System.Drawing.Size(94, 29);
+            this.buttonSauvola.Size = new System.Drawing.Size(82, 22);
             this.buttonSauvola.TabIndex = 4;
             this.buttonSauvola.Text = "Sauvola";
             this.buttonSauvola.UseVisualStyleBackColor = true;
@@ -93,9 +99,10 @@ namespace IMGApp
             // 
             // buttonWolf
             // 
-            this.buttonWolf.Location = new System.Drawing.Point(725, 245);
+            this.buttonWolf.Location = new System.Drawing.Point(634, 184);
+            this.buttonWolf.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonWolf.Name = "buttonWolf";
-            this.buttonWolf.Size = new System.Drawing.Size(94, 29);
+            this.buttonWolf.Size = new System.Drawing.Size(82, 22);
             this.buttonWolf.TabIndex = 5;
             this.buttonWolf.Text = "Wolf";
             this.buttonWolf.UseVisualStyleBackColor = true;
@@ -103,9 +110,10 @@ namespace IMGApp
             // 
             // buttonBradley
             // 
-            this.buttonBradley.Location = new System.Drawing.Point(725, 304);
+            this.buttonBradley.Location = new System.Drawing.Point(634, 228);
+            this.buttonBradley.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonBradley.Name = "buttonBradley";
-            this.buttonBradley.Size = new System.Drawing.Size(94, 29);
+            this.buttonBradley.Size = new System.Drawing.Size(82, 22);
             this.buttonBradley.TabIndex = 6;
             this.buttonBradley.Text = "Bradley";
             this.buttonBradley.UseVisualStyleBackColor = true;
@@ -113,18 +121,28 @@ namespace IMGApp
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(841, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(736, 9);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(694, 691);
+            this.pictureBox2.Size = new System.Drawing.Size(607, 518);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 532);
+            this.progressBar1.Maximum = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1331, 52);
+            this.progressBar1.TabIndex = 8;
+            // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1518, 715);
+            this.ClientSize = new System.Drawing.Size(1386, 665);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.buttonBradley);
             this.Controls.Add(this.buttonWolf);
@@ -133,6 +151,7 @@ namespace IMGApp
             this.Controls.Add(this.buttonOtsu);
             this.Controls.Add(this.buttonGavr);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form3";
             this.Text = "Form3";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -151,5 +170,6 @@ namespace IMGApp
         private System.Windows.Forms.Button buttonWolf;
         private System.Windows.Forms.Button buttonBradley;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
