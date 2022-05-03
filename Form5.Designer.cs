c5990737872f69efcca461f2142588077909d076
Andrey G <gorelow.gore2012@yandex.ru>
2022-05-03 22:21:55 +0300

    Image to Fourier transform + Fourier Image generation

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox_fourier = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_fourier)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(638, 606);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(917, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox_fourier
+            // 
+            this.pictureBox_fourier.Location = new System.Drawing.Point(671, 452);
+            this.pictureBox_fourier.Name = "pictureBox_fourier";
+            this.pictureBox_fourier.Size = new System.Drawing.Size(575, 569);
+            this.pictureBox_fourier.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_fourier.TabIndex = 2;
+            this.pictureBox_fourier.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1271, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(619, 614);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox_fourier);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form5";
             this.Text = "Form5";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_fourier)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox_fourier;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
--- Form5.cs
+++ Form5.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,9 +19,330 @@ namespace IMGApp
             InitializeComponent();
         }
 
+        Bitmap image_original = null;
+        Color[,] image_matrix = null;
+
+        Complex[,] fourier_Red = null;
+        Complex[,] fourier_Green = null;
+        Complex[,] fourier_Blue = null;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            using OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Directory.GetParent("..\\..\\..\\").FullName;
+            openFileDialog.Filter = "Картинки (png, jpg, bmp, gif) |*.png;*.jpg;*.bmp;*.gif|All files (*.*)|*.*";
+            openFileDialog.RestoreDirectory = true;
 
 
 
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (image_original != null)
+                    image_original.Dispose();
 
+                image_original = new Bitmap(openFileDialog.FileName);
+
+                image_matrix = GetImageMatrix(image_original);
+
+                pictureBox1.Image = image_original;
+            }
+        }
+
+        Color[,] GetImageMatrix(Bitmap image)
+        {
+            Color[,] image_matrix = new Color[image_original.Width, image_original.Height];
+
+            for (int i = 0; i < image_original.Width; i++)
+            {
+                for (int j = 0; j < image_original.Height; j++)
+                {
+
+                    image_matrix[i,j] = image_original.GetPixel(i, j);
+
+                }
+            }
+            return image_matrix;
+        }
+
+        void ImageToFourier(Color[,] image) {
+
+            int width = image.GetLength(0);
+            int height = image.GetLength(1);
+
+            //получить комплексный массив каждого канала
+            fourier_Red = new Complex[width, height];
+            fourier_Green = new Complex[width,height];
+            fourier_Blue = new Complex[width, height];
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {                                                  //  //центрирование
+                    fourier_Red[i, j] = new Complex(image[i, j].R   * Math.Pow(-1, i + j), 0);
+                    fourier_Green[i, j] = new Complex(image[i, j].G * Math.Pow(-1, i + j), 0);
+                    fourier_Blue[i, j] = new Complex(image[i, j].B  * Math.Pow(-1, i + j), 0);
+                }
+            }
+
+            //первый проход фурье
+            for (int i = 0; i < width; i++)
+            {
+                Complex[] temp_Red = new Complex[height];
+                Complex[] temp_Green = new Complex[height];
+                Complex[] temp_Blue = new Complex[height];
+
+                for (int j = 0; j < height; j++)
+                {                                           
+                    temp_Red[j] = fourier_Red[i, j]        ;
+                    temp_Green[j] = fourier_Green[i, j]    ;
+                    temp_Blue[j] = fourier_Blue[i, j]      ;
+                }
+
+                temp_Red = DFT(temp_Red);
+                temp_Green = DFT(temp_Green);
+                temp_Blue = DFT(temp_Blue);
+
+                for (int j = 0; j < height; j++)
+                {
+                    fourier_Red[i,j] = temp_Red[j];
+                    fourier_Green[i,j] = temp_Green[j];
+                    fourier_Blue[i,j] = temp_Blue[j];   
+                }
+            }
+
+            //второй проход фурье
+            for (int j = 0; j < height; j++)
+            {
+                Complex[] temp_Red = new Complex[width];
+                Complex[] temp_Green = new Complex[width];
+                Complex[] temp_Blue = new Complex[width];
+
+                for (int i = 0; i < width; i++)
+                {
+                    temp_Red[i] = fourier_Red[i, j];
+                    temp_Green[i] = fourier_Green[i, j];
+                    temp_Blue[i] = fourier_Blue[i, j];
+                }
+                temp_Red = DFT(temp_Red);
+                temp_Green = DFT(temp_Green);
+                temp_Blue = DFT(temp_Blue);
+
+                for (int i = 0; i < width; i++)
+                {
+                    fourier_Red[i, j] = temp_Red[i];
+                    fourier_Green[i, j] = temp_Green[i];
+                    fourier_Blue[i, j] = temp_Blue[i];
+                }
+
+            }
+            return;
+        
+        }
+
+        void FourierToImage(Complex[,] fourier_Red, Complex[,] fourier_Green, Complex[,] fourier_Blue) {
+
+            int width = fourier_Red.GetLength(0);
+            int height = fourier_Red.GetLength(1);
+
+            //получить комплексный массив каждого канала
+            Complex[,] image_Red = new Complex[width, height];
+            Complex[,] image_Green = new Complex[width, height];
+            Complex[,] image_Blue = new Complex[width, height];
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    image_Red[i, j] =   new Complex(fourier_Red[i,j].Real, -fourier_Red[i,j].Imaginary);
+                    image_Green[i, j] = new Complex(fourier_Green[i, j].Real, -fourier_Green[i, j].Imaginary);
+                    image_Blue[i, j] =  new Complex(fourier_Blue[i, j].Real, -fourier_Blue[i, j].Imaginary);
+                }
+            }
+
+            //первый проход фурье
+            for (int i = 0; i < width; i++)
+            {
+                Complex[] temp_Red = new Complex[height];
+                Complex[] temp_Green = new Complex[height];
+                Complex[] temp_Blue = new Complex[height];
+
+                for (int j = 0; j < height; j++)
+                {                                               
+                    temp_Red[j] =   image_Red[i, j]     ;
+                    temp_Green[j] = image_Green[i, j] ;
+                    temp_Blue[j] =  image_Blue[i, j]   ;
+                }
+
+                temp_Red = DFT(temp_Red,    true);
+                temp_Green = DFT(temp_Green,true);
+                temp_Blue = DFT(temp_Blue,  true);
+
+                for (int j = 0; j < height; j++)
+                {
+                    image_Red[i, j] = temp_Red[j];
+                    image_Green[i, j] = temp_Green[j];
+                    image_Blue[i, j] = temp_Blue[j];
+                }
+            }
+
+            //второй проход фурье
+            for (int j = 0; j < height; j++)
+            {
+                Complex[] temp_Red = new Complex[width];
+                Complex[] temp_Green = new Complex[width];
+                Complex[] temp_Blue = new Complex[width];
+
+                for (int i = 0; i < width; i++)
+                {
+                    temp_Red[i] =   image_Red[i, j];
+                    temp_Green[i] = image_Green[i, j];
+                    temp_Blue[i] = image_Blue[i, j];
+                }
+                temp_Red = DFT(temp_Red);
+                temp_Green = DFT(temp_Green);
+                temp_Blue = DFT(temp_Blue);
+
+                for (int i = 0; i < width; i++)
+                {                                      //центрирование         
+                    image_Red[i, j] = temp_Red[i]      * Math.Pow(-1, i+j) ;
+                    image_Green[i, j] = temp_Green[i]  * Math.Pow(-1, i+j) ;
+                    image_Blue[i, j] = temp_Blue[i] * Math.Pow(-1, i + j);
+                }
+
+            }
+
+            Bitmap new_img = new Bitmap(width, height);
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    new_img.SetPixel(i,j,
+                        Color.FromArgb(Clamp((int)image_Red[i, j].Real, 0, 255), Clamp((int)image_Green[i, j].Real, 0, 255), Clamp((int)image_Blue[i, j].Real , 0,255)));
+                }
+            }
+
+            pictureBox2.Image = new_img;
+            pictureBox2.Refresh();
+
+            return;
+
+        }
+
+        Complex[] DFT(Complex[] input_array, bool reverse = false )
+        {
+            int count = input_array.Length;
+
+
+            Complex[] output_array = new Complex[count];
+            for (int u = 0; u < count; u++)
+            {
+
+                double arg = -2.0 * Math.PI * u / count;
+
+                for (int k = 0; k < count; k++)
+                {
+                    output_array[u] += ( (new Complex(Math.Cos(arg * k), Math.Sin(arg * k))) * input_array[k] );
+                }
+
+                if(!reverse)
+                    output_array[u] /= count;   
+
+            }
+
+
+            return output_array;     
+
+        }
+
+
+        void GetFourierImage(Complex[,] Red, Complex[,] Green, Complex[,] Blue)
+        {
+
+            int width = Red.GetLength(0);
+            int height = Red.GetLength(1);
+
+            Bitmap fourier_image = new Bitmap(width, height);
+
+            double max_red =0, max_green=0, max_blue=0;
+
+            for(int i = 0; i < width; i++)
+            {
+                for(int j = 0; j < height; j++)
+                {
+                    //if (max_red < Math.Log(Red[i, j].Magnitude + 1))
+                    //    max_red = Math.Log(Red[i, j].Magnitude + 1);
+                    //
+                    //if (max_green < Math.Log(Green[i, j].Magnitude + 1))
+                    //    max_green = Math.Log(Green[i, j].Magnitude + 1);
+                    //
+                    //if (max_blue < Math.Log(Blue[i, j].Magnitude + 1))
+                    //    max_blue = Math.Log(Blue[i, j].Magnitude + 1);
+
+                    if(max_red < Math.Log(Red[i,j].Imaginary + 1))
+                        max_red = Math.Log(Red[i,j].Imaginary + 1);
+                    
+                    if(max_green < Math.Log(Green[i,j].Imaginary + 1))
+                        max_green = Math.Log(Green[i,j].Imaginary + 1);
+                    
+                    if(max_blue < Math.Log(Blue[i,j].Imaginary + 1))
+                        max_blue = Math.Log(Blue[i,j].Imaginary + 1);
+                }
+            }
+
+            double f_multiplier = 1.0;
+
+            for (int i = 0; i < width; i++)
+            {
+
+
+                for (int j = 0; j < height; j++)
+                {
+                    fourier_image.SetPixel(i,j,
+                        Color.FromArgb(
+                                Clamp((byte)(f_multiplier* (Math.Log( Red[i, j].Magnitude + 1 ))   * 255 / max_red)   , 0, 255),
+                                Clamp((byte)(f_multiplier* (Math.Log( Green[i, j].Magnitude + 1 )) * 255 / max_green) , 0, 255),
+                                Clamp((byte)(f_multiplier* (Math.Log( Blue[i, j].Magnitude + 1 ))  * 255 / max_blue)  , 0, 255)
+                            ));             
+                }
+            }
+
+            pictureBox_fourier.Image = fourier_image;
+            pictureBox_fourier.Refresh();
+
+        }
+
+
+        /// <summary>
+        /// Клэм Гавра
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="val"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static T Clamp<T>(T val, T min, T max) where T : IComparable<T>
+        {
+            if (val.CompareTo(min) < 0) return min;
+            else if (val.CompareTo(max) > 0) return max;
+            else return val;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (image_original == null)
+                return;
+
+            ImageToFourier(image_matrix);
+
+            GetFourierImage(fourier_Red,fourier_Green,fourier_Blue);
+
+           // FourierToImage(fourier_Red, fourier_Green, fourier_Blue);
+
+            return;
+        }
     }
+
+
 }
